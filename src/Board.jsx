1ef8a243adfba14af82a0187c63b6d594408bcd1
manjur0 <sfmanjur21@gmail.com>
2024-08-01 22:21:42 +0600

    toggling the next player x/o

--- src/Board.jsx
+++ src/Board.jsx
@@ -13,28 +13,35 @@ function Square({ value, onSquareClick }) {
 
 function Board() {
   const [square, setSquare] = useState(Array(9).fill(null));
-  const handleClick = () => {
+  const [next, setNext] = useState(true);
+  const handleClick = (idx) => {
     const nextSquare = square.slice();
-    nextSquare[0] = "X";
+    // toggling the next player X/O
+    if (next) {
+      nextSquare[idx] = "X";
+    } else {
+      nextSquare[idx] = "O";
+    }
     setSquare(nextSquare);
+    setNext(!next);
   };
 
   return (
     <div>
       <div className="flex">
-        <Square value={square[0]} onSquareClick={handleClick} />
-        <Square value={square[1]} onSquareClick={handleClick} />
-        <Square value={square[2]} onSquareClick={handleClick} />
+        <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={square[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="flex">
-        <Square value={square[3]} onSquareClick={handleClick} />
-        <Square value={square[4]} onSquareClick={handleClick} />
-        <Square value={square[5]} onSquareClick={handleClick} />
+        <Square value={square[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={square[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="flex">
-        <Square value={square[6]} onSquareClick={handleClick} />
-        <Square value={square[7]} onSquareClick={handleClick} />
-        <Square value={square[8]} onSquareClick={handleClick} />
+        <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={square[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </div>
   );
