79fa2516096620a96082f1b4085d8f2375e83d17
manjur0 <sfmanjur21@gmail.com>
2024-08-02 00:36:35 +0600

    some changes

--- src/Board.jsx
+++ src/Board.jsx
@@ -11,7 +11,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function Board() {
+export function Board() {
   const [square, setSquare] = useState(Array(9).fill(null));
   const [next, setNext] = useState(true);
   // check for winner
@@ -44,29 +44,45 @@ function Board() {
   };
 
   return (
+    <div className="mt-10 flex justify-center gap-3 ">
+      <div>
+        <h1 className="text-2xl font-bold">{status}</h1>
+        <div className="flex">
+          <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={square[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="flex">
+          <Square value={square[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={square[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="flex">
+          <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={square[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+        <button
+          className="text-2xl font-semibold bg-white text-black px-2 mx-2 rounded-sm"
+          onClick={handleReset}
+        >
+          Reset
+        </button>
+      </div>
+      <div>
+        <h1 className="text-2xl font-bold">One Step Back</h1>
+      </div>
+    </div>
+  );
+}
+
+export default function Game() {
+  return (
     <div>
-      <h1 className="text-2xl font-bold">{status}</h1>
-      <div className="flex">
-        <Square value={square[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={square[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={square[2]} onSquareClick={() => handleClick(2)} />
+      <div>
+        <Board />
       </div>
-      <div className="flex">
-        <Square value={square[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={square[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={square[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="flex">
-        <Square value={square[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={square[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={square[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-      <button
-        className="text-2xl font-semibold bg-white text-black px-2 mx-2 rounded-sm"
-        onClick={handleReset}
-      >
-        Reset
-      </button>
+      <div>{/* TBD */}</div>
     </div>
   );
 }
@@ -90,5 +106,3 @@ function calculateWinner(squares) {
   }
   return null;
 }
-
-export default Board;
